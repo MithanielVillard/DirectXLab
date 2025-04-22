64e17618aea150866edff234a3f5ab4b9b76917e
MithanielVillard <mvillardhernandez@gaming.tech>
2025-04-22 20:51:17 +0200

    Added preview image

--- DirectXLab/main.cpp
+++ DirectXLab/main.cpp
@@ -12,7 +12,8 @@
 int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
 {
 	RenderContext::Init();
-	RenderWindow window(L"Hello engine", 900, 500);
+	RenderWindow window(L"Window 1", 900, 500);
+	RenderWindow window2(L"Window 2", 400, 700);
 
 	Shader vs("VertexShader.cso");
 	Shader ps("PixelShader.cso");
@@ -20,6 +21,7 @@ int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmd
 	D12PipelineObject pso(vs, ps, rootSig);
 
 	Camera cam({ 0.0f, 1.0f, -10.0f }, { 0.0f, 1.0f, 0.0f }, window.GetAspectRatio());
+	Camera cam2({ 0.0f, 1.0f, -10.0f }, { 0.0f, 1.0f, 0.0f }, window.GetAspectRatio());
 
 	Geometry cubeGeo = GeometryFactory::CreateCubeGeo();
 	Transform cubeTransform({ 0.0f, 0.0f, 0.0f }, { 0.0f, 45.0f, 0.0f }, { 1.0f, 1.0f, 1.0f });
@@ -28,12 +30,18 @@ int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmd
 	while (window.IsOpen())
 	{
 		window.Update();
+		window2.Update();
 
 		cam.SetAspectRatio(window.GetAspectRatio());
+		cam2.SetAspectRatio(window2.GetAspectRatio());
 
 		window.BeginFrame(cam);
 		window.Draw(cubeGeo, pso, cubeTransform);
 		window.EndFrame();
+
+		window2.BeginFrame(cam2);
+		window2.Draw(cubeGeo, pso, cubeTransform);
+		window2.EndFrame();
 
 		RenderContext::FlushCommandQueue();
 	}
